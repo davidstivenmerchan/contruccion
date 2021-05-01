172b672534f7ad451ff62ecd2272fd0245e794f4
stivenshun <dsmerchan6@misena.edu.co>
2021-05-01 17:21:09 -0500

    cosas

--- Roles/instru/instructor.php
+++ Roles/instru/instructor.php
@@ -1,5 +1,5 @@
 <?php
-  require_once ('../conexion/conexion.php')   
+  require_once ('../../includes/conexion.php')   
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/instru.css">
+    <link rel="stylesheet" href="../../css/instru.css">
     <title>Instructor</title>
 </head>
 <body>
