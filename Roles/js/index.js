e490171c4641a56e2c62c9e5558b3f6644f11129
Lany09 <lmacosta496@misena.edu.co>
2021-05-03 07:41:53 -0500

    Cambios del index

---
+++ Roles/js/index.js
@@ -0,0 +1,6 @@
+function aparecer(){
+    document.getElementById('login').classList.add('login-box-activo')
+}
+function desaparecer(){
+    document.getElementById('login').classList.remove('login-box-activo')
+}
