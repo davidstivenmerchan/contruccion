0677ea0271d7a06f7083a03940b12e96c9d2a20f
Lany09 <lmacosta496@misena.edu.co>
2021-05-08 08:48:48 -0500

    cambios de color

--- Roles/admi/admin.php
+++ Roles/admi/admin.php
@@ -12,7 +12,6 @@ require_once("../../includes/validacion.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
     <link rel="stylesheet" href="./../../css/admin.css">
-    <!-- <link rel="stylesheet" href="./css/calendario.css"> -->
 </head>
 <body>
     <div class="container">
