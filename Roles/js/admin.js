557f00d02782f092cbacf785d7bfe6a6b5da8ed5
aldemar niño cortazar <anino79@misena.edu.co>
2021-05-12 13:51:15 -0500

    cambios en botones

--- Roles/js/admin.js
+++ Roles/js/admin.js
@@ -72,19 +72,17 @@ document.addEventListener('click' , e => {
         const elemento = document.querySelector(`.${primera}`).getAttribute('data-form');
         desaparecer( [ elemento ] );
     }
-
+    // const aparecerEquipo = document.querySelectorAll
     if(e.target.matches('.aparecer')){
         callAparecer(formularios);
     }
     if(e.target.matches('.cerrar')){
         callDesaparecer();
     }
-    if(e.target.matches('.aparecerequipos')){
-        debugger;
+    if(e.target.matches('.aparecerequipos') || e.target.matches('.aparecerequipos *')){
         callAparecer(formula);
     }
     if(e.target.matches('.cerrarequipos')){
-        debugger;
         callDesaparecer();
     }
 
