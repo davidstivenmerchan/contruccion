d14d8a549af112a57f6018710c51eff8ce116c63
Lany09 <lmacosta496@misena.edu.co>
2021-05-12 12:40:04 -0500

    cambios de la pagina principal

---
+++ Roles/admi/pag_admin/form_editar_tip_dispo.php
@@ -0,0 +1,12 @@
+<?php
+    require './../../../includes/conexion.php';
+?>
+
+<form method="POST">
+    <input type="hidden" name="id" value="3">
+    <label for="tipodispo"> ID </label>
+    <input type="text" name="" id="" placeholder="">
+
+    <label for="tip_dispo"> tipo de dispositivo</label>
+    <input type="text" name="tip_dispo" id="tipo_dispo" class="tipo_dispo" id="tipo_dispo">
+</form>
