c64ff29fc386018ef7d11e2d056b25795f287a75
stivenshun <dsmerchan6@misena.edu.co>
2021-06-20 18:40:19 -0500

    equipos de computo cambiar de estado de disponibilidad en el instru

--- Roles/instru/registrar_equipos.php
+++ Roles/instru/registrar_equipos.php
@@ -4,13 +4,57 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/formularios2.css">
-    <link rel="stylesheet" href="./pag_admin/css/equipos.css">
-    <link rel="stylesheet" href="./../../css/tablas.css">
+    <link rel="stylesheet" href="css/equipos.css">
     <title>Document</title>
 </head>
 <body>
 
+<h1>EQUIPOS DE COMPUTO</h1>
 
+<nav>
+    <ul>
+        <li onclick="parte1()";><strong>DISPONIBILIDAD DE LOS EQUIPOS</strong></li>
+        <li onclick="parte2()"><strong>ESTADO DE LOS EQUIPOS</strong></li>
+    </ul>
+</nav>
+
+<div id="disponibilidad">
+  <h2><strong>Disponibilidad de los Equipos</strong></h2>
+
+  <table>
+      <thead>
+          <tr>
+            <th>Serial</th>
+            <th>Placa Sena</th>
+            <th>Tipo de Dispositivo</th>
+            <th>Nombre Dispositivo</th>
+            <th>Marca</th>
+            <th>Estado Disponibilidad</th>
+            <th>Operacion</th>
+            
+          </tr>
+      </thead>
+      <tbody id="tabladisponiblidad">
+
+      </tbody>
+  </table>
+   
+
+
+
+
+
+</div>
+
+
+<div id="estado">
+<h2><strong>Estado de los Equipos</strong></h2>
+</div>
+
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+<script src="js/equipos.js"></script>
 </body>
 </html>
