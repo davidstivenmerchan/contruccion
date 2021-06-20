b4db55965a4d6417da29ca8af76100e667ad2d29
stivenshun <dsmerchan6@misena.edu.co>
2021-06-19 23:33:08 -0500

    coco

--- Roles/aprendiz/aprendiz.php
+++ Roles/aprendiz/aprendiz.php
@@ -5,6 +5,14 @@
 
     $nombre = $_SESSION['nombre'];
     $documento = $_SESSION['cc'];
+    $cod_carnet = $_SESSION['cod_carnet'];
+    $apellido = $_SESSION['apellido'];
+    $fecha =$_SESSION['fecha'];
+    $genero =$_SESSION['genero'];
+    $correo_p =$_SESSION['correo_p'];
+    $correo_s=$_SESSION['correo_s'];
+    $tel=$_SESSION['tel'];
+    $_SESSION['clave'];
 
     $consulta1 = "SELECT entrada_aprendiz.documento, asignacion_equipos.id_equipo, equipos.serial 
     FROM entrada_aprendiz, asignacion_equipos, equipos  
@@ -17,11 +25,18 @@
     if($mostrar1){
         $serial = $mostrar1['serial'];
     }
+
+    $consulta2 = "SELECT asignacion_equipos.id_asignacion_equipos, entrada_aprendiz.documento 
+    from asignacion_equipos, entrada_aprendiz
+    where entrada_aprendiz.id_entrada_aprendiz = asignacion_equipos.id_entrada_aprendiz
+    and entrada_aprendiz.documento='$documento'";
+    $ejecucion2 = mysqli_query($mysqli, $consulta2);
+    $mostrar2 = mysqli_fetch_array($ejecucion2);
+    if($ejecucion2){
+        $id_asignacion = $mostrar2['id_asignacion_equipos'];
+    }
+
 ?>
-
-
-
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -40,35 +55,97 @@
             <img src="../../assets/logo_solo.png" alt="logo construccion" title="logo software">
         </figure>
         <nav>
-            <a href="./inicio.php">inicio</a>
+        <a href="#">Mis Datos</a>
             <a href="#">Mi Computador</a>
-            <a href="./yotampco">Mis Datos Personales</a>
+            
         </nav>
         <article class="opciones">
             <a href="../../includes/cerrar.php"><i class="fas fa-door-open"></i></a>
             <p>Aprendiz</p>
         </article>
     </header>
-    <h2>MIS DATOS PERSONALES</h2>
+    <h2>MIS DATOS</h2>
+    <div class="papa2">
+        <div class="mis_datos">
+            <h3>MIS DATOS PERSONALES</h3>
+            <p>Documento: <?php echo  $documento ?></p>
+            <p>Cod Carnet: <?php echo  $cod_carnet ?></p>
+            <p>Nombres: <?php echo  $nombre ?></p>
+            <p>Apellidos: <?php echo  $apellido ?></p>
+            <p>Fecha de Nacimiento: <?php echo  $fecha ?></p>
+            <p>Correo Personal: <?php echo  $correo_p ?> </p>
+            <p>Correo misena: <?php echo  $correo_s ?></p>
+            <p>Telefono: <?php echo  $tel ?></p>
+        </div>
+        <div class="mis_datos">
+            <h3>DATOS DE MI MATRICULA</h3>
+            <p>Documento: <?php echo  $documento ?></p>
+            <p>Cod Carnet: <?php echo  $cod_carnet ?></p>
+            <p>Nombres: <?php echo  $nombre ?></p>
+            <p>Apellidos: <?php echo  $apellido ?></p>
+            <p>Fecha de Nacimiento: <?php echo  $fecha ?></p>
+            <p>Correo Personal: <?php echo  $correo_p ?> </p>
+            <p>Correo misena: <?php echo  $correo_s ?></p>
+            <p>Telefono: <?php echo  $tel ?></p>
+
+        </div>
 
 
+    </div>
 <hr>
-
     <h2>MI COMPUTADOR</h2>
 
-    <p >Mi Equipo de Computo Asignado: <?php echo $serial ?> </p> 
+    <p class="texto">Mi Equipo de Computo Asignado es: <?php echo $serial ?> </p> 
+<div class="padre">
 
-    <button id="botoninicial"  class="botoninicial" onclick="alerta()">
-        Estado Inicial
-    </button>
-    <button  class="botoninicial">
-        Estado Final
-    </button>
-    <form action="" id="formularioinicio" class="formuu">
-        <input type="text"><input type="text"><input type="text"><input type="text">
-    </form>
+    <div>
+        <button id="botoninicial"  class="botoninicial" onclick="alerta()">
+            Estado Inicial
+        </button>
+        <form action="" id="formularioinicio" class="formuu">
+            <textarea name="" id="mensajeinicio" cols="30" rows="10" max=30 min=30 placeholder="Describa en que Estado Encontro su Equipo de Computo"></textarea>
+            <input type="hidden" value="<?php echo $id_asignacion ?>" id="id_asignacion_inicio">
+            <button id="enviarinicial">
+                Enviar Estado de Equipo
+            </button>
+            
+        </form>
+            <button class="cerrar" id="cerrar" onclick="cerrar()">
+                Cerrar Este Formulario
+            </button>
+    </div>
+    <div>
+        <button  class="botoninicial" onclick="alerta2()">
+            Estado Final
+        </button>
+        <form action="" id="formularioinicioo" class="formuu">
+            <textarea name="" id="" cols="30" rows="10" max=30 min=30 placeholder="Describa en que Estado Dejo su Equipo de Computo"></textarea>
+            <button>
+                Enviar Estado de Equipo
+            </button>
+            
+        </form>
+            <button class="cerrar2" id="cerrarr" onclick="cerrarr()">
+                Cerrar Este Formulario
+            </button>
 
+    </div>
+</div>
+   
+   
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+    
 
+    
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
 
     <script src="js_aprendiz/aprendiz.js"></script>
 </body>
