df438f71f46df10fd5b92af7d52616a8ced42539
stivenshun <dsmerchan6@misena.edu.co>
2021-06-29 10:35:51 -0500

    asignacion funcionando con todos los errores

--- sweetalert/sweetalert.js
+++ sweetalert/sweetalert.js
@@ -1,60 +1,49 @@
-Swal.fire({
-	//title: 'hola karnales',
-	text: 'informacion para alertas en nuestra aplicacion',
-	// html:
-	icon: 'success',
-	confirmButtonText: 'Aceptar',
-	footer: 'informacion extra',
-	// width:
-	// padding:
-	background: 'black'
-	// grow:
-	// backdrop:
-	// timer:
-	// timerProgressBar:
-	// toast:
-	// position:
-	// allowOutsideClick:
-	// allowEscapeKey:
-	// allowEnterKey:
-	// stopKeydownPropagation:
-
-	// input:
-	// inputPlaceholder:
-	// inputValue:
-	// inputOptions:
+export const alertas =(texto) => {
+	Swal.fire({
+		title: 'En Hora Buena',
+		html:`<p class="texto">${texto}</p>`,
+		icon: 'success',
+		confirmButtonText: 'Aceptar',
+		footer: 'informacion extra',
+		width:'40%',
+		height:'100%',
+		// padding:
+		//background: '#ff6b00',
+		customClass: {
+		confirmButton: 'btn-success',
+		container: 'texto',
+		popup: 'cosas',
+		icon: 'ico',
+		image: 'image',
+		content: 'texto',
+		confirmButton: 'boton',
+		cancelButton: 'popo',
+		footer: 'popo',
+		title: 'titul'
+		},
+		backdrop:true,
+		// timer:
+		// timerProgressBar:
+		// toast:
+		// position:
+		// stopKeydownPropagation:
+		// input:
+		// inputPlaceholder:
+		// inputValue:
+		// inputOptions:
+		// showConfirmButton:
+		// confirmButtonAriaLabel:
+		// showCancelButton:
+		// cancelButtonText:
+		// cancelButtonColor:
+		// cancelButtonAriaLabel:
+		buttonsStyling:false,
+		// showCloseButton:
+		// closeButtonAriaLabel:
+		imageUrl:'assets/logo_sl.png',
+		imageWidth: 80
+		// imageHeight:
+		// imageAlt:
+	});
 	
-	//  customClass:
-	// 	container:
-	// 	popup:
-	// 	header:
-	// 	title:
-	// 	closeButton:
-	// 	icon:
-	// 	image:
-	// 	content:
-	// 	input:
-	// 	actions:
-	// 	confirmButton:
-	// 	cancelButton:
-	// 	footer:	
-
-	// showConfirmButton:
-	// confirmButtonColor:
-	// confirmButtonAriaLabel:
-
-	// showCancelButton:
-	// cancelButtonText:
-	// cancelButtonColor:
-	// cancelButtonAriaLabel:
-	
-	// buttonsStyling:
-	// showCloseButton:
-	// closeButtonAriaLabel:
-
-
-	// imageUrl:
-	// imageWidth:
-	// imageHeight:
-	// imageAlt:
-});
+}
