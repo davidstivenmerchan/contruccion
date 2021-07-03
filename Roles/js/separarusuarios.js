a3c7101b25e1547a1a2fa6e1767e1907389461c8
stivenshun <dsmerchan6@misena.edu.co>
2021-07-03 08:08:47 -0500

    cosas

--- Roles/js/separarusuarios.js
+++ Roles/js/separarusuarios.js
@@ -1,13 +1,9 @@
-
-
 function aparecer_Aprendices() {
     $("#tablausu").show();
     $("#tablainstru").hide();
 }
 function aparecer_instructores() {
     
-    
-
     $("#tablainstru").show();
     $("#tablausu").hide();
   
