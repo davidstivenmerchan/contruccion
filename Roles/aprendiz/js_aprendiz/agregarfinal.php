eb7febb4866f6b3ee2d55ee016b8d73d75d06ee7
stivenshun <dsmerchan6@misena.edu.co>
2021-07-06 16:58:53 -0500

    arreglo de algunos errores por los cambios en la bd

--- Roles/aprendiz/js_aprendiz/agregarfinal.php
+++ Roles/aprendiz/js_aprendiz/agregarfinal.php
@@ -20,7 +20,23 @@ if(isset($_POST['enviarfinal'])){
     $consulta2="UPDATE entrada_aprendiz SET hora_salida = '$horaHoy' where documento='$cc'";
     $ejecucion2=mysqli_query($mysqli, $consulta2);
 
-    if(!$ejecicion1 && !$ejecucion2){
+    $consulta3 ="SELECT entrada_aprendiz.documento, equipos.serial from entrada_aprendiz, asignacion_equipos, equipos where entrada_aprendiz.id_entrada_aprendiz=asignacion_equipos.id_entrada_aprendiz
+    and equipos.id_equipo=asignacion_equipos.id_equipo";
+    $ejecucion3 = mysqli_query($mysqli, $consulta3);
+    $mostrar3= mysqli_fetch_array($ejecucion3);
+    if($mostrar3){
+        $serial = $mostrar3['serial'];
+    }
+
+    
+
+    $consulta4 = "UPDATE dispositivo_electronico set dispositivo_electronico.id_estado_disponibilidad=1 
+                 where dispositivo_electronico.serial=$serial";
+    
+    $ejecucion4 = mysqli_query($mysqli, $consulta4);
+
+
+    if(!$ejecicion1 && !$ejecucion2 && !$ejecucion3 && !$ejecucion4 ){
         die('error');
     }else{
         echo "<script> alert('Envío el estado en el que dejo su equipo de computo correctamente!');
