a582cc8c30f2ca206571929b57422009c19c948e
stivenshun <dsmerchan6@misena.edu.co>
2021-07-10 17:33:44 -0500

    opciones de asistencia en reportes

---
+++ Roles/instru/elegir_asistencia.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/elegir_asistencia.css">
+    <title>Reportes</title>
+
+</head>
+<body>
+<div class="titulo">
+    <img src="../../assets/logo_sl.png" alt="">
+    <h1>INFORMES DE ASISTENCIA</h1>
+</div>
+    <div class="padre">
+        <div class="hijo1">
+           <a href="../../reportes/asistencia.php"><div class="hijo11"><p>MES ACTUAL</p></div></a> 
+        </div>
+        <div class="hijo2">
+            <div class="hijo22"> <p>MES ESPECIFICO</p></div>
+        </div>
+    </div>
+    
+</body>
+</html>
