d5d2e682ac8ef8670cb8db7004ba719bb517c12d
stivenshun <dsmerchan6@misena.edu.co>
2021-07-12 12:01:37 -0500

    informe de asignacion listo

---
+++ reportes/asignacion.php
@@ -0,0 +1,66 @@
+<?php
+require('fpdf.php');
+
+require 'proceso_asistencia.php';
+
+$consulta = "SELECT entrada_aprendiz.documento, entrada_aprendiz.fecha, equipos.serial, 
+asignacion_equipos.descripcion_inicial, asignacion_equipos.descripcion_final
+FROM entrada_aprendiz, equipos, asignacion_equipos
+WHERE entrada_aprendiz.id_entrada_aprendiz=asignacion_equipos.id_entrada_aprendiz
+and equipos.id_equipo=asignacion_equipos.id_equipo";
+$ejecucion= $mysqli->query($consulta);
+
+
+class PDF extends FPDF
+{
+// Cabecera de página
+function Header()
+{
+    // Logo
+    $this->Image('../assets/logo_sin_fondo_6.png',10,8,22);
+    // Arial bold 15
+    $this->SetFont('Arial','B',12);
+    
+    // Movernos a la derecha
+    $this->Cell(50);
+    // Título
+    $this->Cell(100,10,'Informes de Asignacion',1,0,'C');
+    // Salto de línea
+    $this->Ln(30);
+
+    $this->Cell(39,10,'Documento',1, 0,'C',0);
+    $this->Cell(39,10,'Fecha',1, 0,'C',0);
+    $this->Cell(39,10,'Serial',1, 0,'C',0);
+    $this->Cell(39,10,'Descripcion Inicial',1, 0,'C',0);
+    $this->Cell(39,10,'Descripcion Final',1, 1,'C',0);
+}
+
+// Pie de página
+function Footer()
+{
+    // Posición: a 1,5 cm del final
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Número de página
+    $this->Cell(0,10,'Pagina'.$this->PageNo().'/{nb}',0,0,'C');
+}
+}
+
+// Creación del objeto de la clase heredada
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Arial','',12);
+
+
+while($row = $ejecucion->fetch_assoc()){
+    $pdf->Cell(39,20,$row['documento'],1, 0,'C',0);
+    $pdf->Cell(39,20,$row['fecha'],1, 0,'C',0);
+    $pdf->Cell(39,20,$row['serial'],1, 0,'C',0);
+    $pdf->Cell(39,20,$row['descripcion_inicial'],1, 0,'C',0);
+    $pdf->Cell(39,20,$row['descripcion_final'],1, 1,'L',0);
+
+}
+$pdf->Output();
+?>
