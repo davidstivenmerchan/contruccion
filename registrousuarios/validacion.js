32bfd01e78f9efe2ce2af75c0497df97dc525564
stivenshun <dsmerchan6@misena.edu.co>
2021-07-16 11:29:36 -0500

    validacion completa registro de aprendices

--- registrousuarios/validacion.js
+++ registrousuarios/validacion.js
@@ -11,6 +11,20 @@ const expresiones = {
 	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 }
 
+const campos = {
+    cedula: false,
+    codigo: false,
+    nombre: false,
+    apellido: false,
+    telefono: false,
+    correo_p: false,
+    corroe_s: false,
+    pass1: false
+
+}
+
+
+
 const validarFormulario = (e) =>{
     switch(e.target.name){
         case "cc":
@@ -18,10 +32,12 @@ const validarFormulario = (e) =>{
 
                 document.getElementById('ccm').classList.remove('cancelcedula__ver');
                 document.getElementById('ccb').classList.add('correccedula__ver');
+                campos['cedula']=true;
 
             }else{
                 document.getElementById('ccb').classList.remove('correccedula__ver');
                 document.getElementById('ccm').classList.add('cancelcedula__ver');
+                campos['cedula']=false;
             }
         break;
 
@@ -30,10 +46,12 @@ const validarFormulario = (e) =>{
 
                 document.getElementById('codm').classList.remove('cancelcarnet__ver');
                 document.getElementById('codb').classList.add('correccarnet__ver');
+                campos['codigo']=true;
 
             }else{
                 document.getElementById('codb').classList.remove('correccarnet__ver');
                 document.getElementById('codm').classList.add('cancelcarnet__ver');
+                campos['codigo']=false;
             }
         break;
 
@@ -42,10 +60,12 @@ const validarFormulario = (e) =>{
 
                 document.getElementById('nomm').classList.remove('cancelnom_ver');
                 document.getElementById('nomb').classList.add('correcnom__ver');
+                campos['nombre']=true;
 
             }else{
                 document.getElementById('nomb').classList.remove('correcnom__ver');
                 document.getElementById('nomm').classList.add('cancelnom_ver');
+                campos['nombre']=false;
             }
 
         break;
@@ -54,10 +74,12 @@ const validarFormulario = (e) =>{
 
                 document.getElementById('apem').classList.remove('cancelape_ver');
                 document.getElementById('apeb').classList.add('correcape__ver');
+                campos['apellido']=true;
 
             }else{
                 document.getElementById('apeb').classList.remove('correcape__ver');
                 document.getElementById('apem').classList.add('cancelape_ver');
+                campos['apellido']=false;
             }
 
         break;
@@ -66,10 +88,12 @@ const validarFormulario = (e) =>{
 
                 document.getElementById('telm').classList.remove('canceltel_ver');
                 document.getElementById('telb').classList.add('correctel__ver');
+                campos['telefono']=true;
 
             }else{
                 document.getElementById('telb').classList.remove('correctel__ver');
                 document.getElementById('telm').classList.add('canceltel_ver');
+                campos['telefono']=false;
             }
 
         break;
@@ -78,10 +102,12 @@ const validarFormulario = (e) =>{
 
                 document.getElementById('cosm').classList.remove('cancelcos_ver');
                 document.getElementById('cosb').classList.add('correccos__ver');
+                campos['corroe_s']=true;
 
             }else{
                 document.getElementById('cosb').classList.remove('correccos__ver');
                 document.getElementById('cosm').classList.add('cancelcos_ver');
+                campos['corroe_s']=false;
             }
 
         break;
@@ -90,10 +116,12 @@ const validarFormulario = (e) =>{
 
                 document.getElementById('copm').classList.remove('cancelcop_ver');
                 document.getElementById('copb').classList.add('correccop__ver');
+                campos['correo_p']=true;
 
             }else{
                 document.getElementById('copb').classList.remove('correccop__ver');
                 document.getElementById('copm').classList.add('cancelcop_ver');
+                campos['correo_p']=false;
             }
 
         break;
@@ -103,10 +131,12 @@ const validarFormulario = (e) =>{
 
                 document.getElementById('pasm').classList.remove('cancelpas_ver');
                 document.getElementById('pasb').classList.add('correcpas__ver');
+                
 
             }else{
                 document.getElementById('pasb').classList.remove('correcpas__ver');
                 document.getElementById('pasm').classList.add('cancelpas_ver');
+                
             }
 
             validarcontraseña();
@@ -130,14 +160,48 @@ const validarcontraseña = ()=>{
 
         document.getElementById('pas2b').classList.remove('correcpas2__ver');
         document.getElementById('pas2m').classList.add('cancelpas2_ver');
+        campos['pass1']=false;
 
     }else{
 
         document.getElementById('pas2m').classList.remove('cancelpas2_ver');
         document.getElementById('pas2b').classList.add('correcpas2__ver');
+        
+        campos['pass1']=true;
 
     }
 }
+
+
+
+
+
+    formulario.addEventListener('submit', (e) =>{
+
+        if(campos.cedula && campos.codigo && campos.nombre && campos.apellido && campos.correo_p && campos.corroe_s && campos.telefono && campos.pass1){
+            
+            console.log('funciona');
+         //   document.getElementById('bloqueo').classList.add('bloqueo_quitar');
+            
+        
+        }else{
+
+            e.preventDefault();
+
+        
+                document.getElementById('bloqueo').classList.add('bloqueo_quitar');
+
+            setTimeout(() => {
+
+                document.getElementById('bloqueo').classList.remove('bloqueo_quitar');
+
+            }, 5000);
+        }
+        
+        
+        
+        
+    })
 
 
 inputs.forEach((inputs)=>{
@@ -145,6 +209,3 @@ inputs.forEach((inputs)=>{
     inputs.addEventListener('blur', validarFormulario);
 })
 
-formulario.addEventListener('submit', (e) =>{
-    e.preventDefault();
-})
